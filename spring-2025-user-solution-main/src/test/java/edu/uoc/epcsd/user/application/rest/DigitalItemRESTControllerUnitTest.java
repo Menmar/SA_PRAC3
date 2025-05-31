7a5f1aa0f8cde86adbbf56b7619ea1b53df06bb3
Menmar <felixmenendezmarques@gmail.com>
2025-05-31 13:06:08 +0200

    DigitalItemRESTControllerUnitTest

---
+++ spring-2025-user-solution-main/src/test/java/edu/uoc/epcsd/user/application/rest/DigitalItemRESTControllerUnitTest.java
@@ -0,0 +1,45 @@
+package edu.uoc.epcsd.user.application.rest;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import edu.uoc.epcsd.user.domain.DigitalItem;
+import edu.uoc.epcsd.user.domain.service.DigitalItemService;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(DigitalItemRESTController.class)
+public class DigitalItemRESTControllerUnitTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private DigitalItemService digitalItemService;
+
+  @Test
+  public void findDigitalItemBySessionTest() throws Exception {
+    Long sessionId = 2L;
+    DigitalItem item = DigitalItem.builder().id(1L).digitalSessionId(1L).description("Test description").lat(1L).lon(1L)
+        .link("http://item.com/test").build();
+
+    List<DigitalItem> expectedItems = Collections.singletonList(item);
+
+    when(digitalItemService.findDigitalItemBySession(sessionId)).thenReturn(expectedItems);
+
+    mockMvc.perform(get("/digitalItem/digitalItemBySession").param("digitalSessionId", sessionId.toString()))
+        .andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].id", is(item.getId().intValue())))
+        .andExpect(jsonPath("$[0].description", is(item.getDescription())))
+        .andExpect(jsonPath("$[0].digitalSessionId", is(item.getDigitalSessionId().intValue())));
+  }
+}
